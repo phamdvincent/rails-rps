12ad4117deaca8f1c4110095e47f0305f1bfdd11
Vincent Pham <40014512+phamdvincent@users.noreply.github.com>
2024-02-06 21:57:30 +0000

    Added competed project

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,19 @@ class PagesController < ApplicationController
   def rules
     render({:template => "pages_templates/rules"})
   end
+
+  def rock
+    @computer_choice = rand(1..3)
+    render({ :template => "pages_templates/rock" })
+  end
+
+  def paper
+    @computer_choice = rand(1..3)
+    render({ :template => "pages_templates/paper" })
+  end
+
+  def scissors
+    @computer_choice = rand(1..3)
+    render({ :template => "pages_templates/scissors" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   get("/home", { :controller => "pages", :action => "home"})
 
   get("/", { :controller => "pages", :action => "rules"})
+  get("/rock", { :controller => "pages", :action => "rock"})
+  get("/paper", { :controller => "pages", :action => "paper"})
+  get("/scissors", { :controller => "pages", :action => "scissors"})
 end
