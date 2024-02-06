7238a6ffe06dcdc30a0440dad52ec4e18ee68e1b
Vincent Pham <40014512+phamdvincent@users.noreply.github.com>
2024-02-06 21:41:18 +0000

    Added rules page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,8 @@ class PagesController < ApplicationController
     @date = Date.today.day
     render({ :template => "pages_templates/home" })
   end
+
+  def rules
+    render({:template => "pages_templates/rules"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get("/admin", { :controller => "admin", :action => "show"})
   get("/home", { :controller => "pages", :action => "home"})
+
+  get("/", { :controller => "pages", :action => "rules"})
 end
